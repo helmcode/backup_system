6479b722c04d2109a549126e72b92f988df86bca
Cristian Cordova <cristian@helmcode.com>
2024-01-19 14:05:14 +0000

    Updated Config File Format && Updated Global config utility

--- utils/config.sh
+++ utils/config.sh
@@ -5,26 +5,36 @@ read_yml() {
 
 load_config() {
     config_path=$1
-
     if [[ -z $config_path ]]
     then
         error_log "Config file not specified"
         exit 1
-    fi
-
-    if [[ ! -f $config_path ]]
+    elif [[ ! -f $config_path ]]
     then
         error_log "Config file not found: $config_path"
         exit 1
     fi
 
-    enable_postgres=$(read_yml '.postgres.enabled' "$config_path")
-    enable_postgres=${enable_postgres,,}
-
-    if [[ $enable_postgres == "true" ]]
+    datasources=$(read_yml '.datasources' "$config_path")
+    if [[ -z $datasources ]]
     then
-        load_postgres_config $config_path
+        error_log "No datasources found in config file"
+        exit 1
+    fi
+
+    jobs=$(read_yml '.jobs' "$config_path")
+    if [[ -z $jobs ]]
+    then
+        error_log "No jobs found in config file"
+        exit 1
+    fi
+
+    postgres_datasources=$(read_yml '.datasources[] | select(.engine == "postgres")' "$config_path")
+    if [[ -z $postgres_datasources ]]
+    then
+        info_log "No postgres datasources found in config file"
     else
-        info_log "Postgres disabled"
+        info_log "Postgres datasources found in config file"
+        load_postgres_config $config_path
     fi
 }
